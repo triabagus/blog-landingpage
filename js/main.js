d430cf4b7e8c77560e79b9d06e562719f94754ea
triabagus <triatop9@gmail.com>
2019-10-04 13:53:12 +0700

    all done responsive design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
 $(document).ready(function () {
 
     $nav = $('.nav');
@@ -18,7 +33,14 @@ $(document).ready(function () {
         navText: [
             $('.owl-navigation .owl-nav-prev'),
             $('.owl-navigation .owl-nav-next'),
-        ]
+        ],
+        responsive: responsive
     });
 
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 5000);
+    });
 });
